786bebb52a5eb4884dc4fa424d6bed20b6cfa152
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-04-03 09:05:04 +0900

    230403 update
    [추가] 사용자 인증 폼 로직 구성

---
+++ 10 react-handing-forms/src/App.js
@@ -0,0 +1,13 @@
+// import SimpleInput from "./components/SimpleInput";
+import BasciForm from "./components/BasicForm";
+
+function App() {
+  return (
+    <div className="app">
+      {/* <SimpleInput /> */}
+      <BasciForm />
+    </div>
+  );
+}
+
+export default App;
