a36e1acb85e251cb539d9c5f72d1431010964a3b
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-04-18 16:34:52 +0900

    230418 update
    [추가] 액션 객체 작동 함수

--- 14 react-diving-into-redux/src/store/index.js
+++ 14 react-diving-into-redux/src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiSlicce from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-  reducer: { ui: uiSlicce.reducer },
+  reducer: { ui: uiSlicce.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
