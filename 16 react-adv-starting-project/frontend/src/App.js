952d0470531310ca33d5fff727855fb802a13c9a
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-04-23 00:03:38 +0900

    230423 update
    [추가]
    1.  json() 유틸리티 함수 이용해서 데이터 처리하기
    2. useRouteLoaderData 훅 및 다른 라우트에서 데이터에 엑세스하기
    3.  action() 함수 사용하기

--- 16 react-adv-starting-project/frontend/src/App.js
+++ 16 react-adv-starting-project/frontend/src/App.js
@@ -2,16 +2,20 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -28,15 +32,23 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-            element: <EventDetailPage />,
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+              },
+            ],
           },
           {
             path: "new",
             element: <NewEventPage />,
-          },
-          {
-            path: ":eventId/edit",
-            element: <EditEventPage />,
+            action: newEventAction,
           },
         ],
       },
